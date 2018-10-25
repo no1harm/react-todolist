9a37cf90791cae2263f0dce7f7b5b96de06b3649
no1harm <chen09869@gmail.com>
2018-10-25 17:11:05 +0800

    添加样式

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import TodoInput from './components/TodoInput'
 import TodoItem from './components/TodoItem'
 import './App.css';
+import 'normalize.css'
+import './reset.css'
 
 class App extends Component {
   constructor(props){
