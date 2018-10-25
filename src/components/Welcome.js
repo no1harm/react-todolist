ca6015b789cca9ad7a6fbd23c50d95caae6061ea
no1harm <chen09869@gmail.com>
2018-10-25 15:08:29 +0800

    生命周期钩子函数

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,9 +9,23 @@ class Welcome extends Component{
         setInterval(()=>{this.setState({
             date: new Date()
         })},1000)
+        console.log('我已经在 constructor 里将 props 和 state 初始化好了')
+    }
+    componentWillMount(){
+        console.log('开始运行 render')
     }
     render(){
+        console.log('正在运行 render')
         return <div><h1>Hello, {this.props.name}</h1><h2>{this.state.date.toString()}</h2></div> ;
+    }
+    componentDidMount(){
+        console.log('render 运行完毕')
+    }
+    componentWillUpdate(){
+        console.log('开始更新组件')
+    }
+    componentDidUpdate(){
+        console.log('更新完毕')
     }
 }
 export default Welcome
