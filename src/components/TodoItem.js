977a98189884182f678a4b53638a86d68b467915
no1harm <chen09869@gmail.com>
2018-10-25 19:09:45 +0800

    添加标记完成/未完成

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class TodoItem extends Component{
     render(){
-        return <li>{this.props.todo.title}</li>
+        return (<div>
+            <input 
+            type="checkbox"
+            checked={this.props.todo.status === 'completed'}
+            onChange={this.toggle.bind(this)}/>
+            {this.props.todo.title}
+        </div>)
+    }
+    toggle(e){
+        this.props.onToggle(e, this.props.todo)
     }
 }
 
