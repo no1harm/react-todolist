535cdf307b5fa95d9beccc61d1d21ec04f218f1f
no1harm <chen09869@gmail.com>
2018-10-25 17:56:05 +0800

    添加todo

--- src/App.js
+++ src/App.js
@@ -10,22 +10,25 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        {id:1, title:'第一个待办'},
-        {id:2, title:'第二个待办'}
+
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
-      return <TodoItem todo={item}/>
+      return (
+        <li key={index}>
+          <TodoItem todo={item}/>
+        </li>
+      )
     })
     return (
       <div className="App">
         <h1>Todo List</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo}/>
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -33,6 +36,24 @@ class App extends Component {
       </div>
     );
   }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0
+ function idMaker(){
+  id += 1
+  return id
+}
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class TodoInput extends Component{
     render(){
-        return <input type='text' defaultValue={this.props.content}/>
+        return <input type='text' defaultValue={this.props.content} onKeyPress={this.submit.bind(this)}/>
+    }
+    submit(e){
+        if(e.key === "Enter"){
+            this.props.onSubmit(e)
+        }
     }
 }
 
