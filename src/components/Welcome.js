b069c17f1c81f5e8aa603aa6f36874b117691b1b
no1harm <chen09869@gmail.com>
2018-10-25 16:13:23 +0800

    使用 setState

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -20,15 +20,12 @@ class Welcome extends Component{
     }
     render(){
         console.log('正在运行 render')
-        this.setState({
-            test:'render'
-        })
-        return 
+        return ( 
             <div>
             <h1>Hello, {this.props.name}</h1>
             <h2>{this.state.date.toString()}</h2>
             <h3>{this.state.test}</h3>
-            </div> ;
+            </div>)
     }
     componentDidMount(){
         this.setState({
@@ -37,23 +34,12 @@ class Welcome extends Component{
         console.log('render 运行完毕')
     }
     shouldComponentUpdate(){
-        this.setState({
-          date: new Date(), 
-          test: 'shouldComponentUpdate'
-        })
+        return true
       }
     componentWillUpdate(){
-        this.setState({
-            date: new Date(), // 更新 date
-            test: 'componentWillUpdate'
-          })
         console.log('开始更新组件')
     }
     componentDidUpdate(){
-        this.setState({
-            date: new Date(), // 更新 date
-            test: 'componentDidUpdate'
-          })
         console.log('更新完毕')
     }
 }
