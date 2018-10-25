7534bc8b7b64a0069f05528fa74e122420c5d11c
no1harm <chen09869@gmail.com>
2018-10-25 14:00:47 +0800

    state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 class Welcome extends Component{
+    constructor(){
+        super()
+        this.state = {
+            date: new Date()
+        }
+    }
     render(){
-        return <h1>Hello, {this.props.name}</h1>;
+        return <div><h1>Hello, {this.props.name}</h1><h2>{this.state.date.toString()}</h2></div> ;
     }
 }
 export default Welcome
