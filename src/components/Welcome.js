22e2851433622411f3930749d9f337650fe5c874
no1harm <chen09869@gmail.com>
2018-10-25 13:43:12 +0800

    props 接受数据

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Welcome extends Component{
     render(){
-        return <h1>Hello, Component</h1>;
+        return <h1>Hello, {this.props.name}</h1>;
     }
 }
 export default Welcome
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Welcome from './components/Welcome'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Welcome/>,
+  <Welcome name="no1harm"/>,
   document.getElementById('root')
 )
 
