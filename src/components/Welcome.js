d63986653cc489e360223e7856fd143cdb5a8d47
no1harm <chen09869@gmail.com>
2018-10-25 14:43:25 +0800

    setState 改变state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,6 +6,9 @@ class Welcome extends Component{
         this.state = {
             date: new Date()
         }
+        setInterval(()=>{this.setState({
+            date: new Date()
+        })},1000)
     }
     render(){
         return <div><h1>Hello, {this.props.name}</h1><h2>{this.state.date.toString()}</h2></div> ;
