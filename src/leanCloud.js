084ef355a7b98fc4628aa597a396da0157dd1bed
no1harm <chen09869@gmail.com>
2018-10-26 14:46:54 +0800

    引入 leanCloud，准备添加登录注册功能

---
+++ src/leanCloud.js
@@ -0,0 +1,11 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'RPWGaqvaJi7fUh3HBoT6TqP4-gzGzoHsz';
+var APP_KEY = '520Y7aeHCJycPnBzxymEb2Ya';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
