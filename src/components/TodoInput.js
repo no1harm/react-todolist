8aa378952ea31c49b680562ef3d2121ee2ad2174
no1harm <chen09869@gmail.com>
2018-11-01 20:41:33 +0800

    引入 animaiton.css

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import '../css/TodoInput.css'
+import "animate.css";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 class TodoInput extends Component{
     constructor(props) {
@@ -20,21 +22,29 @@ class TodoInput extends Component{
       }
     render(){
         return (
-        <form className='InputForm'>
-            <label>
-                <input
-                className='Inputstyle'
-                name='title'
-                ref="title"
-                placeholder="  添加 Todo"
-                style={{ maxWidth: 200 }}
-                value={this.props.content} 
-                onKeyPress={this.submit.bind(this)}
-                onChange={this.handleInputChange}
-                />
-            </label>
-            <Button onClick={this.submit2.bind(this)}>添加</Button>
-        </form>)
+            <ReactCSSTransitionGroup
+            transitionEnter={true}
+            transitionLeave={true}
+            transitionEnterTimeout={2500}
+            transitionLeaveTimeout={1500}
+            transitionName="animated"
+            >
+            <form className='InputForm animated bounceInDown' key="amache" >
+                <label>
+                    <input
+                    className='Inputstyle'
+                    name='title'
+                    ref="title"
+                    placeholder="  添加 Todo"
+                    style={{ maxWidth: 200 }}
+                    value={this.props.content} 
+                    onKeyPress={this.submit.bind(this)}
+                    onChange={this.handleInputChange}
+                    />
+                </label>
+                <Button onClick={this.submit2.bind(this)}>添加</Button>
+            </form>
+        </ReactCSSTransitionGroup>)
     }
     submit(e){
         if(e.key === 'Enter'){
